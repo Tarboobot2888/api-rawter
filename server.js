6316ca3d9d489bcb78ad6ca0f65123e21dd7daaa
Tarboobot2888 <129686276+Tarboobot2888@users.noreply.github.com>
2025-04-16 14:18:38 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,22 +4,17 @@ const fs = require('fs');
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json()); // دعم JSON
+app.use(express.json());
 app.set('json spaces', 2);
-app.listen(9012, () => {
-  console.log(`Server running on port 9012`);
-});
+
 const routesDir = path.join(__dirname, 'routes');
 const apiList = [];
 global.t = 'https://takamura-api.joanimi-world.site';
-// تحميل جميع الـ API ديناميكيًا
+
 fs.readdirSync(routesDir).forEach((file) => {
   const route = require(path.join(routesDir, file));
-
   if (route.path && route.router) {
     app.use(route.path, route.router);
-
-    // إضافة تفاصيل الـ API إلى القائمة
     apiList.push({
       name: route.name || file.replace('.js', '').toUpperCase(),
       type: route.type || 'default',
@@ -31,15 +26,12 @@ fs.readdirSync(routesDir).forEach((file) => {
   }
 });
 
-// إرجاع قائمة الـ API كـ JSON
 app.get('/api/list', (req, res) => {
   res.json(apiList);
 });
 
-// التعامل مع الروابط غير المعروفة
 app.use((req, res) => {
   res.status(404).json({ error: 'Not Found' });
 });
 
-// تصدير التطبيق لـ Vercel
 module.exports = app;
